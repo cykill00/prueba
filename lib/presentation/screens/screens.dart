5795b294f63b56b5374b2b4ceb148348ff24160e
Pepe <ereisy@gmail.com>
2023-08-31 17:26:14 -0400

    Screen creadas y barril actualizado

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,14 @@
+export 'package:widgets_app/presentation/screens/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial_screen.dart';
+export 'package:widgets_app/presentation/screens/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar.dart';
+export 'package:widgets_app/presentation/screens/ui_controls_screen.dart';
 export 'package:widgets_app/presentation/screens/bottons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home_screen.dart';
+
+
+
+
+
---
+++ lib/presentation/screens/ui_controls_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UiControlScreen extends StatelessWidget {
+  const UiControlScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
